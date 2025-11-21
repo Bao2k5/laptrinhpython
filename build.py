3587d88cdd9f60376c21ae574ad8a33a046f7785
Bao2k5 <leduongbao2019@gmail.com>
2025-11-21 23:11:53 +0700

    Add build.py for Render deployment

---
+++ build.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Build script for Render deployment
+API server doesn't need to build game - just install dependencies
+"""
+
+print("="*50)
+print("API SERVER BUILD")
+print("="*50)
+print("\nNo build steps needed for API server.")
+print("Dependencies will be installed automatically from requirements.txt")
+print("\n✓ Build complete!")
