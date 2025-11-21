1f1addaabdad4479e39744d8bd6716fa711f4c23
Bao2k5 <leduongbao2019@gmail.com>
2025-11-21 16:41:07 +0700

    Build game tren server thay vi commit file build

--- build.py
+++ build.py
@@ -1,19 +1,2 @@
-import os
-import sys
-
-def build():
-    print("--- Bắt đầu cài đặt thư viện ---")
-    
-    # Cài đặt thư viện
-    print("Đang cài đặt thư viện từ requirements.txt...")
-    exit_code = os.system("pip install -r requirements.txt")
-    if exit_code != 0:
-        print("Lỗi khi cài đặt thư viện!")
-        sys.exit(1)
-        
-    print("--- Hoàn tất! ---")
-    print("Lưu ý: File game đã được build sẵn trong thư mục build/web")
-
-if __name__ == "__main__":
     build()
 
